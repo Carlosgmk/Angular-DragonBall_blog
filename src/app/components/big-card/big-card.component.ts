1faef826812d2131bdb4cc2be603a602682750aa
Macedo <gm18242@gmail.com>
2023-10-17 21:55:58 -0300

    criação do Conten e pj em produção

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -12,9 +12,10 @@ export class BigCardComponent implements OnInit {
   @Input()
   cardTitle:string = ""
   @Input()
-  cardDescription:string =""
-  
-  
+  cardDescription:string = ""
+  @Input()
+  Id:string="0"
+
   constructor() {}
 
   ngOnInit(): void {
---
+++ src/app/data/dataFk.ts
@@ -0,0 +1,46 @@
+export const dataF = [
+     {
+          "id":"1",
+         
+          "title": "Curiosidades Vegeta",
+          "description": "opa",
+          "photoCover":"https://i.pinimg.com/736x/c8/b1/bb/c8b1bb3114c672f38590186a4215fe23.jpg"
+     },
+     {
+          "id":"2",
+          "title": "Broly",
+          "description": "",
+          "photoCover":"https://revolucaonerd.com/wordpress/wp-content/files/revolucaonerd.com/2023/04/broly.jpg  "
+     },
+     {
+          "id":"3",
+          "title": "Gohan",
+          "description": "https://revolucaonerd.com/wordpress/wp-content/files/revolucaonerd.com/2022/08/gohan-1280x720.webp",
+          "photoCover":""
+     },
+     {
+          "id":"4",
+          "title": "Trunks",
+          "description": "",
+          "photoCover":"https://observatoriodocinema.uol.com.br/wp-content/plugins/seox-image-magick/imagick_convert.php?width=1200&height=900&format=webp&quality=91&imagick=/wp-content/uploads/2022/12/trunks-del-futur_1902800913.jpg"
+     },
+     {
+          "id":"5",
+          "title": "Goku Black",
+          "description": "",
+          "photoCover":"https://criticalhits.com.br/wp-content/uploads/2020/12/Super-Saiyajin-Rose.jpg"
+     },
+     {
+          "id":"6",
+          "title": "Oozaru",
+          "description": "",
+          "photoCover":" https://www.mundodeportivo.com/alfabeta/hero/2021/01/gohan.jpg?width=768&aspect_ratio=16:9&format=nowebp"
+     },
+
+
+
+
+
+
+
+]
